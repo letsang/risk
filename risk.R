71be4583333005bde50d686f75504d4af1098562
letsang <tsang.jacques@gmail.com>
2020-05-12 01:25:18 +0200

    update 12/05

--- risk.R
+++ risk.R
@@ -4,15 +4,18 @@ library(leaflet)
 library(firebase)
 
 ui <- bootstrapPage(
-  tags$style(type = "text/css", "html, body {width:100%;height:100%}
-             #controls{background-color:white;padding:20px;}"),
-  
+  # tags$img(src="https://raw.githubusercontent.com/letsang/risk/master/logorisk.png", height ="25px"),
+  tags$style(type = "text/css", "html, body {width:100%; height:100%}
+                    #controls {background-color:white; opacity:0.5; padding:20px;}
+                    #controls:hover {opacity:0.8;}"),
   useFirebase(), # import dependencies
   useFirebaseUI(),
   leafletOutput('map', height = '100%', width = '100%'),
-# reqSignin will hide the content until the user signs in, but not safe.
-  reqSignin(absolutePanel(top = 10, right = 10, id = "controls",
-                checkboxInput("legend", "Show legend", TRUE)))
+  # reqSignin will hide the content until the user signs in, but not safe.
+  reqSignin(absolutePanel(tags$img(src="https://raw.githubusercontent.com/letsang/risk/master/logorisk.png", height ="25px"),
+                          top = 10, right = 10, id = "controls",
+                          checkboxInput("legend", "Show legend", TRUE))
+  )
 )
 
 server <- function(input, output){
