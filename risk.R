07c332685d9a815054076d79588157fbcf48a089
letsang <tsang.jacques@gmail.com>
2020-05-11 22:26:32 +0200

    update 11/05

--- risk.R
+++ risk.R
@@ -3,21 +3,16 @@ library(shiny)
 library(leaflet)
 library(firebase)
 
-mainUI <- function() {
-  bootstrapPage(
-    theme = shinythemes::shinytheme('simplex'),
-    leaflet::leafletOutput('map', height = '1000px', width = '100%'),
-    absolutePanel(top = 10, left = 10, id = 'controls'),
-    tags$style(type = "text/css", "html, body {width:100%;height:100%}
-                   #controls{background-color:white;padding:20px;}")
-  )
-}
-
-ui <- fluidPage(
+ui <- bootstrapPage(
+  tags$style(type = "text/css", "html, body {width:100%;height:100%}
+             #controls{background-color:white;padding:20px;}"),
+  
   useFirebase(), # import dependencies
   useFirebaseUI(),
-  reqSignin(h4("RISK")), # hide from UI
-  htmlOutput("screen")
+  leafletOutput('map', height = '100%', width = '100%'),
+# reqSignin will hide the content until the user signs in, but not safe.
+  reqSignin(absolutePanel(top = 10, right = 10, id = "controls",
+                checkboxInput("legend", "Show legend", TRUE)))
 )
 
 server <- function(input, output){
@@ -28,15 +23,15 @@ server <- function(input, output){
   
   output$screen <- renderUI({
     f$req_sign_in() # require sign in
-    div(do.call(bootstrapPage, c("", mainUI())))
+    mainUI()
   })
   
   output$map <- renderLeaflet({
     f$req_sign_in() # require sign in
     leaflet() %>%
-    addProviderTiles("Stamen.Watercolor", options = providerTileOptions(minZoom = 2, maxZoom = 10)) %>%
-    setView(lng = 0, lat = 40, zoom = 3) %>%
-    setMaxBounds(lng1 = -180, lat1 = 90, lng2 = 180, lat2 = -90)
+      addProviderTiles("Stamen.Watercolor", options = providerTileOptions(minZoom = 2, maxZoom = 10)) %>%
+      setView(lng = 0, lat = 40, zoom = 3) %>%
+      setMaxBounds(lng1 = -180, lat1 = 90, lng2 = 180, lat2 = -90)
   })
 }
 
