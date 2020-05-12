bc09e211f82906b3fcaf05814438c89f975b12eb
letsang <tsang.jacques@gmail.com>
2020-05-12 16:32:41 +0200

    update 12/05

--- risk.R
+++ risk.R
@@ -2,9 +2,11 @@ library(dplyr)
 library(shiny)
 library(leaflet)
 library(firebase)
+library(sf)
+
+regionMap <- spData::world %>% group_by(subregion) %>% summarize()
 
 ui <- bootstrapPage(
-  # tags$img(src="https://raw.githubusercontent.com/letsang/risk/master/logorisk.png", height ="25px"),
   tags$style(type = "text/css", "html, body {width:100%; height:100%}
                     #controls {background-color:white; opacity:0.5; padding:20px;}
                     #controls:hover {opacity:0.8;}"),
@@ -12,9 +14,10 @@ ui <- bootstrapPage(
   useFirebaseUI(),
   leafletOutput('map', height = '100%', width = '100%'),
   # reqSignin will hide the content until the user signs in, but not safe.
-  reqSignin(absolutePanel(tags$img(src="https://raw.githubusercontent.com/letsang/risk/master/logorisk.png", height ="25px"),
+  reqSignin(absolutePanel(tags$img(src="https://raw.githubusercontent.com/letsang/risk/master/logorisk2.png", height ="25px"),
                           top = 10, right = 10, id = "controls",
-                          checkboxInput("legend", "Show legend", TRUE))
+                          checkboxInput("legend", "Show legend", TRUE)
+                          )
   )
 )
 
@@ -24,17 +27,15 @@ server <- function(input, output){
     set_providers(email = TRUE)$
     launch() # launch
   
-  output$screen <- renderUI({
-    f$req_sign_in() # require sign in
-    mainUI()
-  })
-  
   output$map <- renderLeaflet({
     f$req_sign_in() # require sign in
     leaflet() %>%
       addProviderTiles("Stamen.Watercolor", options = providerTileOptions(minZoom = 2, maxZoom = 10)) %>%
       setView(lng = 0, lat = 40, zoom = 3) %>%
-      setMaxBounds(lng1 = -180, lat1 = 90, lng2 = 180, lat2 = -90)
+      setMaxBounds(lng1 = -180, lat1 = 90, lng2 = 180, lat2 = -90) %>% 
+      addPolygons(data = regionMap, weight = 0, color = "white", smoothFactor = 0.000001,
+                  fillColor = "black", fillOpacity = 0.1,
+                  highlightOptions = highlightOptions(fillColor = "white", bringToFront = TRUE))
   })
 }
 
